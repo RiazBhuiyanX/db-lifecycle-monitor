852f94aa7c87f062aa4618dd217b4be06ff8b3e0
Riaz Bhuiyan <riaz.s.bhuiyan@gmail.com>
2026-03-01 15:31:57 +0200

    feat: implement SLF4J and Logback for professional observability

--- src/main/java/bg/riaz/Main.java
+++ src/main/java/bg/riaz/Main.java
@@ -1,10 +1,14 @@
 package bg.riaz;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -17,11 +21,11 @@ public class Main {
 
         String url = "jdbc:postgresql://localhost:" + port + "/" + dbName;
 
-        System.out.println("Опит за свързване към: " + url);
+        logger.info("Опит за свързване към: {}", url);
 
         if (dbName == null) {
-            System.err.println("Липсва .env файл или конфигурация!");
-            System.err.println("Моля, копирайте .env.example към .env и попълнете данните.");
+            logger.error("Липсва .env файл или конфигурация!");
+            logger.error("Моля, копирайте .env.example към .env и попълнете данните.");
             System.exit(1);
         }
 
@@ -31,7 +35,7 @@ public class Main {
             dbManager.performFullCheck();
 
             try {
-                System.out.println("Изчакване 10 секунди...");
+                logger.info("Изчакване 10 секунди...");
                 Thread.sleep(10000);
             } catch (InterruptedException e) {
                 break;
